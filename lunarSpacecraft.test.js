47e2d3e2c6d0e73d932c9e1cd696121d45f28418
dixitt5 <tilajidixit@gmail.com>
2023-08-29 23:54:37 +0530

    Added failing tests for various movement and turning commands.

--- lunarSpacecraft.test.js
+++ lunarSpacecraft.test.js
@@ -8,8 +8,38 @@ test("initializes spacecraft with right direction and position", () => {
   expect(spacecraft.direction).toBe("N");
 });
 
-test('moves spacecraft forward when direction is N', () => {
-    const spacecraft = new lunarSpacecraft(0, 0, 0, 'N');
-    spacecraft.move('f');
-    expect(spacecraft.y).toBe(1);
+test("moves spacecraft forward when direction is N", () => {
+  const spacecraft = new lunarSpacecraft(0, 0, 0, "N");
+  spacecraft.move("f");
+  expect(spacecraft.y).toBe(1);
+});
+
+test("moves spacecraft backward when direction is N", () => {
+  const spacecraft = new lunarSpacecraft(0, 0, 0, "N");
+  spacecraft.move("b");
+  expect(spacecraft.y).toBe(-1);
+});
+
+test("turns spacecraft right from N direction", () => {
+  const spacecraft = new lunarSpacecraft(0, 0, 0, "N");
+  spacecraft.move("r");
+  expect(spacecraft.direction).toBe("E");
+});
+
+test("turns spacecraft left from N direction", () => {
+  const spacecraft = new lunarSpacecraft(0, 0, 0, "N");
+  spacecraft.move("l");
+  expect(spacecraft.direction).toBe("W");
+});
+
+test("turns spacecraft up from N direction", () => {
+  const spacecraft = new lunarSpacecraft(0, 0, 0, "N");
+  spacecraft.move("u");
+  expect(spacecraft.direction).toBe("U");
+});
+
+test("turns spacecraft down from N direction", () => {
+  const spacecraft = new lunarSpacecraft(0, 0, 0, "N");
+  spacecraft.move("d");
+  expect(spacecraft.direction).toBe("D");
 });
