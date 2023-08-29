1c19c401b20a7cacf1f29185b948b7658a28795b
dixitt5 <tilajidixit@gmail.com>
2023-08-29 23:47:16 +0530

    Added failing test for forward movement.

--- lunarSpacecraft.test.js
+++ lunarSpacecraft.test.js
@@ -7,3 +7,9 @@ test("initializes spacecraft with right direction and position", () => {
   expect(spacecraft.z).toBe(0);
   expect(spacecraft.direction).toBe("N");
 });
+
+test('moves spacecraft forward when direction is N', () => {
+    const spacecraft = new lunarSpacecraft(0, 0, 0, 'N');
+    spacecraft.move('f');
+    expect(spacecraft.y).toBe(1);
+});
