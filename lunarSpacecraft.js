8b53a83f96c586d88c2f1466017932f1c454641d
dixitt5 <tilajidixit@gmail.com>
2023-08-29 23:49:18 +0530

    Implemented basic movement functionality to move in direction N.

--- lunarSpacecraft.js
+++ lunarSpacecraft.js
@@ -1,10 +1,16 @@
 class Spacecraft {
-    constructor(x, y, z, direction) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.direction = direction;
+  constructor(x, y, z, direction) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.direction = direction;
+  }
+
+  move(command) {
+    if (command === "f" && this.direction === "N") {
+      this.y++;
     }
+  }
 }
 
 module.exports = Spacecraft;
