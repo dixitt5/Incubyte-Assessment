b691c3cb78b6324558ced2eabf267312f8e292cc
dixitt5 <tilajidixit@gmail.com>
2023-08-29 23:58:11 +0530

    Implemented more movement logic for all directions and added additional logic for failing tests

--- lunarSpacecraft.js
+++ lunarSpacecraft.js
@@ -7,9 +7,114 @@ class Spacecraft {
   }
 
   move(command) {
-    if (command === "f" && this.direction === "N") {
-      this.y++;
+    switch (command) {
+      case "f":
+        this.forward();
+        break;
+      case "b":
+        this.backward();
+        break;
+      case "l":
+        this.turnLeft();
+        break;
+      case "r":
+        this.turnRight();
+        break;
+      case "u":
+        this.turnUp();
+        break;
+      case "d":
+        this.turnDown();
+        break;
     }
+  }
+
+  forward() {
+    switch (this.direction) {
+      case "N":
+        this.y++;
+        break;
+      case "E":
+        this.x++;
+        break;
+      case "S":
+        this.y--;
+        break;
+      case "W":
+        this.x--;
+        break;
+      case "U":
+        this.z++;
+        break;
+      case "D":
+        this.z--;
+        break;
+    }
+  }
+
+  backward() {
+    switch (this.direction) {
+      case "N":
+        this.y--;
+        break;
+      case "E":
+        this.x--;
+        break;
+      case "S":
+        this.y++;
+        break;
+      case "W":
+        this.x++;
+        break;
+      case "U":
+        this.z--;
+        break;
+      case "D":
+        this.z++;
+        break;
+    }
+  }
+
+  turnLeft() {
+    switch (this.direction) {
+      case "N":
+        this.direction = "W";
+        break;
+      case "E":
+        this.direction = "N";
+        break;
+      case "S":
+        this.direction = "E";
+        break;
+      case "W":
+        this.direction = "S";
+        break;
+    }
+  }
+
+  turnRight() {
+    switch (this.direction) {
+      case "N":
+        this.direction = "E";
+        break;
+      case "E":
+        this.direction = "S";
+        break;
+      case "S":
+        this.direction = "W";
+        break;
+      case "W":
+        this.direction = "N";
+        break;
+    }
+  }
+
+  turnUp() {
+    this.direction = "U";
+  }
+
+  turnDown() {
+    this.direction = "D";
   }
 }
 
