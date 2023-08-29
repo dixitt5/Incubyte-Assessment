7b938e3091d66579d691a2cf2a02f1c76c2ca799
dixitt5 <tilajidixit@gmail.com>
2023-08-30 01:05:57 +0530

    added comments for clarity

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
+// Importing required modules
 const readline = require("readline");
 const Spacecraft = require("./lunarSpacecraft.js");
 
+// Creating a readline interface for user input
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+// Asking the user to specify the initial direction of the spacecraft
 rl.question(
   "Please specify the initial direction which can be N, S, E, W, U, D: ",
   (initialDirection) => {
+    // Asking the user to provide commands for the spacecraft
     rl.question(
       "Please enter the commands in an order of working (separated by commas, e.g., f,r,u,b,l): ",
       (input) => {
-        // Convert the string input into an array of commands
+        // Convert the string input into an array of commands and remove any white spaces
         const commands = input.split(",").map((command) => command.trim());
 
-        const spacecraft = new Spacecraft(0, 0, 0, initialDirection); // Initialize spacecraft with provided initial conditions
+        // Initialize the spacecraft object with initial position (0,0,0) and user-defined direction
+        const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
 
-        // Process commands
+        // Process and execute each command
         commands.forEach((command) => spacecraft.move(command));
 
+        // Output the final position and direction of the spacecraft
         console.log(
           `Final Position: (${spacecraft.x}, ${spacecraft.y}, ${spacecraft.z})`
         );
         console.log(`Final Direction: ${spacecraft.direction}`);
 
+        // Close the readline interface
         rl.close();
       }
     );
--- lunarSpacecraft.js
+++ lunarSpacecraft.js
@@ -1,133 +1,168 @@
-class Spacecraft {
-  constructor(x, y, z, direction) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.direction = direction;
-  }
-
-  move(command) {
-    switch (command) {
-      case "f":
-        this.forward();
-        break;
-      case "b":
-        this.backward();
-        break;
-      case "l":
-        this.turnLeft();
-        break;
-      case "r":
-        this.turnRight();
-        break;
-      case "u":
-        this.turnUp();
-        break;
-      case "d":
-        this.turnDown();
-        break;
+/**
+ * Class representing a lunar spacecraft that moves in three-dimensional space
+ * with the ability to change its direction.
+ */
+class LunarSpacecraft {
+    /**
+     * Create a spacecraft.
+     * @param {number} x - The initial x-coordinate.
+     * @param {number} y - The initial y-coordinate.
+     * @param {number} z - The initial z-coordinate.
+     * @param {string} direction - The initial direction of the spacecraft (N, S, E, W, U, D).
+     */
+    constructor(x, y, z, direction) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.direction = direction;
+    }
+  
+    /**
+     * Process a command to move or change direction of the spacecraft.
+     * @param {string} command - The command to process (f, b, l, r, u, d).
+     */
+    move(command) {
+      switch (command) {
+        case "f":
+          this.forward();
+          break;
+        case "b":
+          this.backward();
+          break;
+        case "l":
+          this.turnLeft();
+          break;
+        case "r":
+          this.turnRight();
+          break;
+        case "u":
+          this.turnUp();
+          break;
+        case "d":
+          this.turnDown();
+          break;
+      }
+    }
+  
+    /**
+     * Move the spacecraft forward based on its current direction.
+     */
+    forward() {
+      switch (this.direction) {
+        case "N":
+          this.y++;
+          break;
+        case "E":
+          this.x++;
+          break;
+        case "S":
+          this.y--;
+          break;
+        case "W":
+          this.x--;
+          break;
+        case "U":
+          this.z++;
+          break;
+        case "D":
+          this.z--;
+          break;
+      }
+    }
+  
+    /**
+     * Move the spacecraft backward based on its current direction.
+     */
+    backward() {
+      switch (this.direction) {
+        case "N":
+          this.y--;
+          break;
+        case "E":
+          this.x--;
+          break;
+        case "S":
+          this.y++;
+          break;
+        case "W":
+          this.x++;
+          break;
+        case "U":
+          this.z--;
+          break;
+        case "D":
+          this.z++;
+          break;
+      }
+    }
+  
+    /**
+     * Turn the spacecraft left based on its current direction.
+     */
+    turnLeft() {
+      switch (this.direction) {
+        case "N":
+          this.direction = "W";
+          break;
+        case "E":
+          this.direction = "N";
+          break;
+        case "S":
+          this.direction = "E";
+          break;
+        case "W":
+          this.direction = "S";
+          break;
+        case "U":
+          this.direction = "N"; // Change in Orientation when facing Up
+          break;
+        case "D":
+          this.direction = "S"; // Change in Orientation when facing Down
+          break;
+      }
+    }
+  
+    /**
+     * Turn the spacecraft right based on its current direction.
+     */
+    turnRight() {
+      switch (this.direction) {
+        case "N":
+          this.direction = "E";
+          break;
+        case "E":
+          this.direction = "S";
+          break;
+        case "S":
+          this.direction = "W";
+          break;
+        case "W":
+          this.direction = "N";
+          break;
+        case "U":
+          this.direction = "S"; // Change in Orientation when facing Up
+          break;
+        case "D":
+          this.direction = "N"; // Change in Orientation when facing Down
+          break;
+      }
+    }
+  
+    /**
+     * Turn the spacecraft to face upwards.
+     */
+    turnUp() {
+      this.direction = "U";
+    }
+  
+    /**
+     * Turn the spacecraft to face downwards.
+     */
+    turnDown() {
+      this.direction = "D";
     }
   }
-
-  forward() {
-    switch (this.direction) {
-      case "N":
-        this.y++;
-        break;
-      case "E":
-        this.x++;
-        break;
-      case "S":
-        this.y--;
-        break;
-      case "W":
-        this.x--;
-        break;
-      case "U":
-        this.z++;
-        break;
-      case "D":
-        this.z--;
-        break;
-    }
-  }
-
-  backward() {
-    switch (this.direction) {
-      case "N":
-        this.y--;
-        break;
-      case "E":
-        this.x--;
-        break;
-      case "S":
-        this.y++;
-        break;
-      case "W":
-        this.x++;
-        break;
-      case "U":
-        this.z--;
-        break;
-      case "D":
-        this.z++;
-        break;
-    }
-  }
-
-  turnLeft() {
-    switch (this.direction) {
-      case "N":
-        this.direction = "W";
-        break;
-      case "E":
-        this.direction = "N";
-        break;
-      case "S":
-        this.direction = "E";
-        break;
-      case "W":
-        this.direction = "S";
-        break;
-      case "U":
-        this.direction = "N";
-        break; // Change in Orientation when facing Up
-      case "D":
-        this.direction = "S";
-        break; // Change in Orientation when facing Down
-    }
-  }
-
-  turnRight() {
-    switch (this.direction) {
-      case "N":
-        this.direction = "E";
-        break;
-      case "E":
-        this.direction = "S";
-        break;
-      case "S":
-        this.direction = "W";
-        break;
-      case "W":
-        this.direction = "N";
-        break;
-      case "U":
-        this.direction = "S";
-        break; // Change in Orientation when facing Up
-      case "D":
-        this.direction = "N";
-        break; // Change in Orientation when facing Down
-    }
-  }
-
-  turnUp() {
-    this.direction = "U";
-  }
-
-  turnDown() {
-    this.direction = "D";
-  }
-}
-
-module.exports = Spacecraft;
+  
+  // Exporting the Spacecraft class to be used in other modules.
+  module.exports = LunarSpacecraft;
+  
